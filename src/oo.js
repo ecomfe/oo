88c8156c5f3399a03d166576788f33ccad72606d
Exodia <d_xinxin@163.com>
2014-05-06 14:37:35 +0800

    简单实现，未测试

---
+++ src/oo.js
@@ -0,0 +1,178 @@
+// TODO:
+// - test case
+// - demo
+void function (define) {
+
+    define(function () {
+        var slice = [].slice;
+
+        function Class() {
+            return Class.create.apply(Class, slice.call(arguments));
+        }
+        /**
+         * @class Class
+         * 简单的 js oo 库
+         *
+         * 用法:
+         *      @example
+         *      // 来个简单的基类，最简单的是 var Super = Class();
+         *      var Super = Class({
+         *          superProp1: 'superProp1',
+         *          superProp2: 'superProp2',
+         *          method: function(){
+         *              alert(this.superProp1);
+         *          },
+         *          superMethod2: function(){
+         *              alert(this.superProp2);
+         *          }
+         *      })
+         *
+         *      // 来个派生类，继承 Super
+         *      var Sub = Class(Super, {
+         *          // constructor 会在实例化时调用
+         *          constructor: function(prop){
+         *             // $super 会自动调用父类的同名方法
+         *             this.$super(arguments)
+         *             this.subProp = prop
+         *             alert("Sub init")
+         *          },
+         *          method: function(){
+         *              this.$super(arguments);
+         *              alert(this.subProp);
+         *          }
+         *      })
+         *
+         *      var Sub1 = Class(Sub, {
+         *          constructor: function(prop1, prop2){
+         *             this.$super(arguments)
+         *             this.sub1Prop = prop2;
+         *             alert("Sub1 init")
+         *          },
+         *          method: function(){
+         *              this.$super(arguments)
+         *              alert(this.sub1Prop)
+         *          }
+         *      })
+         *
+         *      var superIns = new Super();
+         *      var sub = new Sub('Sub'); // alert: Super init, Sub init
+         *      var sub1 = new Sub1('Sub', 'Sub1') // alert: Super init, Sub init, Sub1 init
+         *      superIns.method() // alert: superProp1
+         *      sub.method() // alert: superProp1, Sub
+         *      sub1.method() // alert: superProp1, Sub, Sub1
+         *      sub.superMethod() // alert: superProp2
+         *      sub1.superMethod() // alert: superProp2
+         *
+         */
+        Class.create = function (BaseClass, overrides) {
+            overrides = overrides || {};
+            if (typeof BaseClass == 'object') {
+                // BaseClass 可以是 null
+                overrides = BaseClass || overrides;
+                BaseClass = Class;
+            }
+
+            var kclass = inherit(BaseClass);
+            var proto = kclass.prototype;
+            each(overrides, function (value, key) {
+                if (typeof value == 'function') {
+                    value.__name__ = key;
+                    value.__owner__ = kclass;
+                }
+
+                proto[key] = value;
+            });
+
+            kclass.toString = toString();
+
+            return kclass;
+        };
+
+        Class.toString = function () {
+            return 'function Class() { [native code] }';
+        };
+
+        Class.prototype = {
+            constructor: Class,
+            $self: Class,
+            $superClass: Object,
+            $super: function (args) {
+                var method = this.$super.caller;
+                var name = method.__name__;
+                var superClass = method.__owner__.$superClass;
+                var superMethod = superClass.prototype[name];
+
+                if (typeof superMethod !== 'function') {
+                    throw "Call the super class's " + name + ", but it is not a function!";
+                }
+
+                return superMethod.apply(this, args);
+            }
+        };
+
+        return Class;
+    });
+
+    function inherit(BaseClass) {
+        var kclass = function () {
+            return this.constructor !== kclass &&
+                typeof this.constructor == 'function' &&
+                this.constructor.apply(this, arguments);
+        };
+
+        var Empty = function () { };
+        Empty.prototype = BaseClass.prototype;
+
+        var proto = kclass.prototype = new Empty();
+        proto.$self = kclass;
+        kclass.$superClass = BaseClass;
+
+        return kclass;
+    }
+
+
+    var nativeEach = [].forEach;
+    var hasEnumBug = !({toString: 1}['propertyIsEnumerable']('toString'));
+    var enumProperties = [
+        'constructor',
+        'hasOwnProperty',
+        'isPrototypeOf',
+        'propertyIsEnumerable',
+        'toString',
+        'toLocaleString',
+        'valueOf'
+    ];
+
+    function each(obj, fn) {
+        if (obj.forEach && obj.forEach === nativeEach) {
+            return obj.forEach(fn);
+        }
+
+        var i = 0;
+        if (Object.prototype.toString.call(obj) == '[object Array]') {
+            for (var len = obj.length; i < len; ++i) {
+                fn(obj[i], i, obj);
+            }
+        } else {
+            for (var k in obj) {
+                hasOwnProperty(obj, k) && fn(obj[k], k, obj);
+            }
+            //ie6-8 enum bug
+            if (hasEnumBug) {
+                for (i = enumProperties.length - 1; i > -1; --i) {
+                    var key = enumProperties[i];
+                    hasOwnProperty(obj, key) && fn(obj[key], key, obj);
+                }
+            }
+        }
+    }
+
+    function hasOwnProperty(obj, key) {
+        return Object.prototype.hasOwnProperty.call(obj, key);
+    }
+
+    function toString() {
+        return this.prototype.constructor.toString();
+    }
+
+}(typeof define === 'function' && define.amd ? define : function (factory) { module.exports = factory(require); });
