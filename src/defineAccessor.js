5ef00ce43975c20cd56593189375d91bc77c06ed
dengxinxin <dengxinxin@baidu.com>
2014-08-28 12:03:34 +0800

    加些注释，自定义accessor 时，不默认生成 accessor

--- src/defineAccessor.js
+++ src/defineAccessor.js
@@ -16,12 +16,28 @@ void function (define, undefined) {
                 this[ATTRIBUTES][name] = value;
             }
 
+            /**
+             * 根据指定的属性名生成对应的accessor
+             *
+             * @param {Object | Function} obj 需要生成 accessor 的对象
+             * @param {string} name 需要生成 accessor 的属性名称
+             * @param {Object | Function} [accessor] 自定义的 getter 和 setter 配置
+             * @param {Function} [accessor.get] 自定义的 getter 函数, 配置了accessor，但未设置 get，则不会生成 getter
+             * @param {Function} [accessor.set] 自定义的 setter 函数, 配置了accessor，但未设置 set，则不会生成 setter
+             */
             return function (obj, name, accessor) {
                 var upperName = name.charAt(0).toUpperCase() + name.slice(1);
                 var getter = 'get' + upperName;
                 var setter = 'set' + upperName;
-                obj[getter] = !accessor || typeof accessor.get !== 'function' ? simpleGetter : accessor.get;
-                obj[setter] = !accessor || typeof accessor.set !== 'function' ? simpleSetter : accessor.set;
+
+                if (!accessor) {
+                    obj[getter] = !accessor || typeof accessor.get !== 'function' ? simpleGetter : accessor.get;
+                    obj[setter] = !accessor || typeof accessor.set !== 'function' ? simpleSetter : accessor.set;
+                }
+                else {
+                    typeof accessor.get === 'function' && (obj[getter] = accessor.get);
+                    typeof accessor.set === 'function' && (obj[setter] = accessor.set);
+                }
             };
         }
     );
