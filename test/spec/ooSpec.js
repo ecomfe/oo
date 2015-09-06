e46f956492a875fe38b70f620164ec41aa8b6362
Exodia <d_xinxin@163.com>
2015-09-06 23:21:56 +0800

    增加 defineAccessor 测试用例

--- test/spec/ooSpec.js
+++ test/spec/ooSpec.js
@@ -317,6 +317,50 @@ define(
                 });
             });
 
+            describe('Class.defineAccessor', function () {
+                it('simple accessor', function () {
+                    var obj = {};
+                    Class.defineAccessor(obj, 'name');
+                    obj.setName('test');
+                    expect(obj.getName()).toEqual('test');
+                });
+
+                it('simple accessor', function () {
+                    var obj = {};
+                    var getCounter = 0;
+                    var setCounter = 0;
+                    Class.defineAccessor(obj, 'name', {
+                        set: function (value) {
+                            ++setCounter;
+                            this.name = value;
+                        },
+                        get: function () {
+                            ++getCounter;
+                            return this.name;
+                        }
+                    });
+                    Class.defineAccessor(obj, 'getCounter', {
+                        get: function () {
+                            return getCounter;
+                        }
+                    });
+                    Class.defineAccessor(obj, 'setCounter', {
+                        get: function () {
+                            return setCounter;
+                        }
+                    });
+                    obj.setName('test');
+                    expect(obj.getName()).toEqual('test');
+                    expect(obj.getGetCounter()).toEqual(1);
+                    expect(obj.getSetCounter()).toEqual(1);
+
+                    obj.setName('test2');
+                    expect(obj.getName()).toEqual('test2');
+                    expect(obj.getGetCounter()).toEqual(2);
+                    expect(obj.getSetCounter()).toEqual(2);
+                });
+            });
+
             describe('Class.defineMembers', function () {
                 var Super = Class();
                 var Sub = Class(Super);
