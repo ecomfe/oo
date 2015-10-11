aef0a33b04fa1d3697d83a6c76f1b0253598cf13
Exodia <d_xinxin@163.com>
2015-10-12 00:04:56 +0800

    增加文档

--- src/private.js
+++ src/private.js
@@ -10,6 +10,16 @@
             var inheritObject = require('./static');
             var uuid = 0;
 
+            /**
+             * create a private object whose prototype points the first param if has,
+             * and return a token function which accept an object as param,
+             * call the token function with an object, it will return the private part of the object,
+             * because the private part is just a object, you can do any operation on it.
+             *
+             * @param {Object} [prototype] the private properties on the prototype, all instance will share them.
+             * @return {Function} a token function which accept an object,
+             * it will return the private part of the object, and do not expose the token function to the outside.
+             */
             return function createPrivate(prototype) {
                 var token = ++uuid;
                 return function getPrivate(instance) {
